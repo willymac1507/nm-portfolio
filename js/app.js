2df126d432c4c1dd43668b9bd9ebf27b82f5fcee
Will McCloy <william.mccloy@googlemail.com>
2022-02-03 13:25:47 +0000

    Add animation to hero

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
-$('.hamburger').click(function () {
-    $('.navbar').slideToggle(500, function () {
-        if ($('.navbar').attr('style') === 'display: none;') {
-            $('.navbar').removeAttr('style');
-        };
-    });
-    $('.hamburger').toggleClass('hamburger--open');
-})
+const hamburger = $(".hamburger");
+const navLink = $(".navbar__link");
+
+hamburger.click(function () {
+	$(".navbar").slideToggle(500, function () {
+		if ($(".navbar").attr("style") === "display: none;") {
+			$(".navbar").removeAttr("style");
+		}
+	});
+	hamburger.toggleClass("hamburger--open");
+});
+
+navLink.click(function () {
+	if (hamburger.is(":visible")) {
+		$(".navbar").slideToggle(500, function () {
+			if ($(".navbar").attr("style") === "display: none;") {
+				$(".navbar").removeAttr("style");
+			}
+		});
+		hamburger.toggleClass("hamburger--open");
+	}
+});
+
+$(".hero__headline").hide({ queue: true }).delay(2000).show({ queue: true });
+
+$(".hero__subtitle").hide({ queue: true }).delay(5000).show({ queue: true, complete: function () {
+    $('.animate-typing').delay(500).hide({queue: true});
+} });
+
+$(".animsition").animsition({
+	inDuration: 200,
+	outDuration: 800,
+});
