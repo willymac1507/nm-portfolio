d5b1f4a317b35b09152d9d1828ad797f5dd59a41
Will McCloy <william.mccloy@googlemail.com>
2022-02-08 10:33:25 +0000

    Add form validation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const hamburger = $(".hamburger");
 const navLink = $(".navbar__link");
 
+$(document).ready(function () {
+	validate();
+	$("#fname, #lname, #email, #subject, #message").keyup(validate);
+});
+
+$('.success').hide();
+
 hamburger.click(function () {
 	$(".navbar").slideToggle(500, function () {
 		if ($(".navbar").attr("style") === "display: none;") {
@@ -38,21 +45,26 @@ $(".animsition").animsition({
 	outDuration: 800,
 });
 
-$(document).ready(function () {
-	validate();
-	$("#fname, #lname, #email, #subject, #message").change(validate);
+$('#contactForm').submit(function () {
+	$("#fname, #lname, #email, #subject, #message").val('');
+	$('.success').slideDown().delay(2000).slideUp();
+	$('#contact__submit').attr('disabled', true).addClass('button--disabled');
 });
 
 function validate() {
+	const email = $('#email').val();
+	const pattern = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)
 	if (
 		$("#fname").val().length > 0 &&
 		$("#lname").val().length > 0 &&
 		$("#email").val().length > 0 &&
 		$("#message").val().length > 0 &&
-		$("#subject").val().length > 0
+		$("#subject").val().length > 0 &&
+		pattern.test(email)
 	) {
-		$("#contact__submit").attr("disabled", false);
+		$("#contact__submit").attr("disabled", false).removeClass("button--disabled");
 	} else {
-		$("#contact__submit").attr("disabled", true);
+		$("#contact__submit").attr("disabled", true).addClass("button--disabled");
 	}
-}
+};
+
